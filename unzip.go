c238cc7ec11c7508caeddabe450181c85291757d
blackhat <56522998+blacklat@users.noreply.github.com>
2020-04-02 20:20:27 -0300

    Create unzip.go

---
+++ unzip.go
@@ -0,0 +1,64 @@
+package unzip
+
+import (
+    "os"
+    "io"
+    "fmt"
+    "strings"
+    "archive/zip"
+    "path/filepath"
+)
+
+type Unzip struct {
+    Src     string
+    Dst     string
+}
+
+func New(src string, dst string) Unzip {
+    return Unzip{
+        Src:    src,
+        Dst:    dst,
+    }
+}
+
+func Extract(src string, dst string) ([]string, error) {
+    var filenames []string
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return nil, err
+    }
+    defer r.Close()
+    for f := range r.File {
+        dstpath := filepath.Join(dst, r.File[f].Name)
+        if !strings.HasPrefix(dstpath, filepath.Clean(dst) + string(os.PathSeparator)) {
+            return nil, fmt.Errorf("%s: illegal file path", src)
+        }
+        if r.File[f].FileInfo().IsDir() {
+            if err := os.MkdirAll(dstpath, os.ModePerm); err != nil {
+                return nil, err
+            }
+        } else {
+            if rc, err := r.File[f].Open(); err != nil {
+                return nil, err
+            } else {
+                defer rc.Close()
+                if of, err := os.OpenFile(dstpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, r.File[f].Mode()); err != nil {
+                    return nil, err
+                } else {
+                    defer of.Close()
+                    if _, err = io.Copy(of, rc); err != nil {
+                        return nil, err
+                    } else {
+                        of.Close()
+                        rc.Close()
+                        filenames = append(filenames, dstpath)
+                    }
+                }
+            }
+        }
+    }
+    if len(filenames) == 0 {
+        return nil, fmt.Errorf("zip file is empty")
+    }
+    return filenames, nil
+}
